b335eb7a21b63d1d4856c7092f3cca4013a72209
Tamas Nyiri <info@nyiritamas.hu>
2020-09-15 20:32:37 +0200

    q9

--- ldbc_snb_grblas/loader.py
+++ ldbc_snb_grblas/loader.py
@@ -1,7 +1,36 @@
 import csv
 import errno
+
+from grblas import dtypes
+from itertools import repeat
+
+import logging
+from collections import namedtuple
 from os import strerror
 from os import path
+
+from grblas.matrix import Matrix
+
+
+class LoadError(Exception):  # fixme
+    pass
+
+
+logger = logging.getLogger(__name__)
+
+VertexType = namedtuple('VertexType', [
+    'name',
+    'index2id',  # logical (dense) id -> original (sparse) id
+    'id2index',  # original (sparse) id -> logical (dense) id
+    'data',
+    'length'  # nr. of vertices present
+])
+
+
+DEFAULT_DELIMITER = '|'
+DEFAULT_QUOTE = '"'
+
+ID_NAME = 'id'
 
 
 class Loader:
@@ -16,35 +45,141 @@ class Loader:
         self.data_dir = data_dir
         self.filename_suffix = filename_suffix
 
-    def load_vertex(self, vertex_name):
+    def _parse_header(self, header, column_names):
+        """
+        Gets index for columns based on the header.
+        :param header: list of header string values.
+        :param column_names: list of string values that are needed.
+        :return: list of indexes of needed values in header.
+        """
+        columns = []
+
+        for name in column_names:
+            try:
+                index = header.index(name)
+                columns.append(index)
+
+                # Delete content for this header as it should not be found again.
+                # The possibility for finding it again exists, as e.g. comment<-replyOf-comment relation has
+                # Comment.id twice in the header.
+                header[index] = ''
+            except AttributeError:
+                logger.error("Column '%s' not found! Possible values are: %s" % (
+                    name, ', '.join(header)))
+                raise LoadError()
+
+        return columns
+
+    def load_vertex_type(self, vertex_type_name: str, column_names=[], *, is_dynamic):
         """
 
-        :param vertex_name:
+        :param vertex_type_name:
+        :param column_names:
+        :param is_dynamic:
         :return:
         """
-        raise NotImplementedError()
-
-    def load_edge(self, from_vertex, edge_name, to_vertex, *, is_dynamic):
-        """
-
-        :param edge_name:
-        :param from_vertex:
-        :param to_vertex:
-        :return:
-        """
-
-        filename = "%s_%s_%s%s" % (from_vertex, edge_name, to_vertex, self.filename_suffix)
+        filename = "%s%s" % (vertex_type_name, self.filename_suffix)
         subdir = 'dynamic' if is_dynamic else 'static'
-        full_path = path.join(self.data_dir, subdir, filename)
+        file_path = path.join(self.data_dir, subdir, filename)
 
-        if not path.isfile(full_path):
-            pass
+        with open(file_path) as csvfile:
+            reader = csv.reader(csvfile, delimiter=DEFAULT_DELIMITER, quotechar=DEFAULT_QUOTE)
 
-        with open(full_path) as csvfile:
-            reader = csv.reader(csvfile, delimiter='|', quotechar='"')
+            header = next(reader)
 
-            # skip header
-            next(reader)
+            # determine index of all needed fields, and add index as well
+            column_names.insert(0, ID_NAME)
+            columns = self._parse_header(header, column_names)
 
-            for row in reader:
-                pass
+            mapping = []  # logical (dense) id -> original (sparse) id
+            revese_mapping = {}  # original id -> logical id
+            data = []  # any additional data based on 'column_names'
+
+            for i, row in enumerate(reader):
+                row_data = [row[i] for i in columns]
+                index = int(row_data.pop(0))
+                mapping.append(index)
+                revese_mapping[index] = i
+
+                # if there's anything else to store
+                if row_data:
+                    data.append(row_data)
+
+            return VertexType(vertex_type_name, mapping, revese_mapping, data, len(mapping))
+
+    def load_edge_type(self, from_vertex_type: VertexType, edge_name: str, to_vertex_type: VertexType,
+                       *, is_dynamic: bool, dtype=dtypes.INT32, lmask=None, rmask=None):
+        """
+
+        TODO: add parsing of properties of a relation.
+        :param edge_name:
+        :param from_vertex_type:
+        :param to_vertex_type:
+        :param is_dynamic:
+        :return: adjacency matrix
+        """
+
+        # concat full filename for input file
+        filename = "%s_%s_%s%s" % (from_vertex_type.name, edge_name, to_vertex_type.name, self.filename_suffix)
+
+        # concat file path
+        subdir = 'dynamic' if is_dynamic else 'static'
+        file_path = path.join(self.data_dir, subdir, filename)
+
+        if not path.isfile(file_path):
+            pass  # fixme should we handle this...? open() will raise an exception...
+
+        with open(file_path) as csvfile:
+            reader = csv.reader(csvfile, delimiter=DEFAULT_DELIMITER, quotechar=DEFAULT_QUOTE)
+
+            # get id columns
+            # todo: if attributes are needed, column_names should be a function parameter and
+            # todo: these values should be inserted into that
+            column_names = [
+                '%s.id' % from_vertex_type.name.title(),
+                '%s.id' % to_vertex_type.name.title(),
+            ]
+
+            header = next(reader)
+            columns = self._parse_header(header, column_names)
+
+            from_indexes = []
+            to_indexes = []
+
+            for i, row in enumerate(reader):
+                row_data = [row[i] for i in columns]
+                id_from = int(row_data.pop(0))
+                id_to = int(row_data.pop(0))
+
+                # check if both sides of the connection is present
+                if (id_from not in from_vertex_type.id2index or
+                        id_to not in to_vertex_type.id2index):
+                    logger.info("Dropping dangling edge: (%s:%s)-[%s]-(%s:%s)" % (
+                        from_vertex_type.name, id_from, edge_name, to_vertex_type.name, id_to
+                    ))
+                    continue
+
+                from_index = from_vertex_type.id2index[id_from]
+                if lmask is not None and from_index not in lmask:
+                    continue
+
+                to_index = to_vertex_type.id2index[id_to]
+                if rmask is not None and to_index not in rmask:
+                    continue
+
+                from_indexes.append(from_index)
+                to_indexes.append(to_index)
+
+                # if there's anything else to store
+                if row_data:
+                    # todo: save additional properties
+                    pass
+
+        m = Matrix.from_values(from_indexes, to_indexes,
+                               repeat(1, len(from_indexes)),  # True for all value
+                               nrows=len(from_vertex_type.index2id),
+                               ncols=len(to_vertex_type.index2id),
+                               dtype=dtype,
+                               name="%s_%s_%s" % (from_vertex_type.name, edge_name, to_vertex_type))
+
+        return m
---
+++ ldbc_snb_grblas/q9.py
@@ -0,0 +1,66 @@
+from datetime import datetime
+
+from itertools import islice
+from pytz import UTC
+
+from ldbc_snb_grblas.loader import Loader
+
+
+def _get_date_mask(vertex_type, start_date, end_date):
+    # FIXME
+    mask_indexes = set()
+    for i, [creation_date_str] in enumerate(vertex_type.data):
+        creation_date = datetime.fromisoformat(creation_date_str)
+        if start_date <= creation_date <= end_date:
+            mask_indexes.add(i)
+
+    return mask_indexes
+
+
+def calc_q9(start_date, end_date):
+    loader = Loader(data_dir)
+    persons = loader.load_vertex_type('person', is_dynamic=True, column_names=['firstName', 'lastName'])
+    comments = loader.load_vertex_type('comment', is_dynamic=True, column_names=['creationDate'])
+    posts = loader.load_vertex_type('post', is_dynamic=True, column_names=['creationDate'])
+
+    # get masks
+    comments_mask = _get_date_mask(comments, start_date, end_date)
+    posts_mask = _get_date_mask(posts, start_date, end_date)
+
+    post_hascreator_person = loader.load_edge_type(posts, 'hasCreator', persons, is_dynamic=True, lmask=posts_mask)
+    comment_replyof_post = loader.load_edge_type(comments, 'replyOf', posts, is_dynamic=True, lmask=comments_mask, rmask=posts_mask)
+    comment_replyof_comment = loader.load_edge_type(comments, 'replyOf', comments, is_dynamic=True, lmask=comments_mask, rmask=comments_mask)
+
+    # get number of posts (initiated threads) per persons
+    thread_count = post_hascreator_person.reduce_columns().new()
+
+    # get direct replies for each post as a person-comment matrix
+    m_person_comment = post_hascreator_person.T.mxm(comment_replyof_post.T).new()
+
+    # calculate number of direct comments for persons
+    vec_person = thread_count.ewise_add(m_person_comment.reduce_rows().new()).new()
+
+    # get all comments iteratively per person
+    while m_person_comment.nvals > 0:
+        # get next comments
+        m_person_comment << m_person_comment.mxm(comment_replyof_comment.T)
+
+        # accumulate results
+        vec_person << vec_person.ewise_add(m_person_comment.reduce_rows().new())
+
+    # sort results by message_count
+    sorted_result = sorted(zip(*vec_person.to_values()), key=lambda x: (-x[1], persons.index2id[x[0]]))  # fixme
+
+    # print results
+    for person_index, message_count in islice(sorted_result, 100):
+        first_name = persons.data[person_index][0]
+        last_name = persons.data[person_index][1]
+        person_id = persons.index2id[person_index]
+        print(person_id, first_name, last_name, thread_count[person_index].value, message_count)
+
+
+# fixme call function...
+data_dir = '/home/nyirit/projects/dipterv/social_network_20_03_03/'  # fixme
+start_date = datetime(2012, 5, 31, tzinfo=UTC)
+end_date = datetime(2012, 6, 30, tzinfo=UTC)
+calc_q9(start_date, end_date)
